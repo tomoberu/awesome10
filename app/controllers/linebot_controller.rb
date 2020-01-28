0a26934bf321395ac5e64b3a6c1546c809bc129f
tomoberu <57937124+tomoberu@users.noreply.github.com>
2020-01-28 16:09:20 +0900

    Update linebot_controller.rb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -13,17 +13,17 @@ class LinebotController < ApplicationController
     array2 = ["がんば！","飲んで飲んで飲んで！","いってらっしゃーい！","いい波のってんねぇ！","もう一回まわすドン！"]
 
     events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: "#{event.message['text']}ちゃん！おめでとう！\nテキーラ#{p array1[rand(5)]}杯です！\n#{p array2[rand(5)]}"
-          }
-        end
-      end
-      client.reply_message(event['replyToken'], message)
+      # case event
+      # when Line::Bot::Event::Message
+      #   case event.type
+      #   when Line::Bot::Event::MessageType::Text
+      #     message = {
+      #       type: 'text',
+      #       text: "#{event.message['text']}ちゃん！おめでとう！\nテキーラ#{p array1[rand(5)]}杯です！\n#{p array2[rand(5)]}"
+      #     }
+      #   end
+      # end
+      # client.reply_message(event['replyToken'], message)
 
       if event.message['text'] != nil
         place = event.message['text'] #ここでLINEで送った文章を取得
